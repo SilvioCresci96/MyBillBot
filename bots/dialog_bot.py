fd314a190be8deeea45da198b6e5b6a3530a0d8d
SilvioCresci <silviocresci1996@gmail.com>
2021-03-17 13:16:19 +0100

    welcome message

--- bots/dialog_bot.py
+++ bots/dialog_bot.py
@@ -70,9 +70,7 @@ class DialogBot(ActivityHandler):
             if member.id != turn_context.activity.recipient.id:
                 
                 await turn_context.send_activity(
-                    "Welcome to the Proactive Bot sample.  Navigate to "
-                    "http://localhost:3978/api/notify to proactively message everyone "
-                    "who has previously messaged this bot."
+                    "Benvenuto in MyBillBot. Attraverso questo bot è possibile tenere traccia del proprio fatturato e richiedere in qualsiasi momento il fatturato di uno specifico giorno o di un certo periodo.\nAll'inizio della conversazione si hanno 3 opzioni:\n1. Aggiungere fattura manualmente: vi sarà richiesto di inserire l'importo da salvare e la data da associare a tale importo.\n2. Aggiungi foto fattura: vi sarà richiesto di inviare una foto dello scontrino di chiusura cassa in modo che provvederò io a ricavarmi l'importo e la data da salvare.\n3. Operazione: in questa sezione puoi chiedermi il fatturato di un giorno specifico o il fatturato di un intero mese. Puoi usare questa sezione anche per richiedere che venga eliminata una fattura in una particolare data in modo che non ne terrò conto per calcoli futuri.\n\n Inizia subito a tenere traccia del tuo fatturato!"
                 )
     
     def _add_conversation_reference(self, activity: Activity):
--- helpers/dialog_helper.py
+++ helpers/dialog_helper.py
@@ -24,7 +24,7 @@ class DialogHelper:
         
         elif turn_context.activity.text == "/start":
             await turn_context.send_activity(MessageFactory.attachment(CardFactory.hero_card(HeroCard(
-                text="Wecolme ecc.."
+                text="Benvenuto in MyBillBot. Attraverso questo bot è possibile tenere traccia del proprio fatturato e richiedere in qualsiasi momento il fatturato di uno specifico giorno o di un certo periodo.\nAll'inizio della conversazione si hanno 3 opzioni:\n1. Aggiungere fattura manualmente: vi sarà richiesto di inserire l'importo da salvare e la data da associare a tale importo.\n2. Aggiungi foto fattura: vi sarà richiesto di inviare una foto dello scontrino di chiusura cassa in modo che provvederò io a ricavarmi l'importo e la data da salvare.\n3. Operazione: in questa sezione puoi chiedermi il fatturato di un giorno specifico o il fatturato di un intero mese. Puoi usare questa sezione anche per richiedere che venga eliminata una fattura in una particolare data in modo che non ne terrò conto per calcoli futuri.\n\n Inizia subito a tenere traccia del tuo fatturato! "
             ))))
         else:
             results = await dialog_context.continue_dialog()
