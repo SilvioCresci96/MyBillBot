7efa483489caeb11ba24c5b76633f139b08cdf03
SilvioCresci <silviocresci1996@gmail.com>
2021-03-18 14:17:14 +0100

    help

--- dialogs/waterfall_main.py
+++ dialogs/waterfall_main.py
@@ -87,6 +87,9 @@ class WaterfallMain(ComponentDialog):
         elif result == "Operazioni":
             
             return await step_context.begin_dialog(WaterfallQuery.__name__)
+        elif result == "Help":
+            await step_context.context.send_activity("Benvenuto in MyBillBot. Attraverso questo bot è possibile tenere traccia del proprio fatturato e richiedere in qualsiasi momento il fatturato di uno specifico giorno o di un certo periodo.\nAll'inizio della conversazione si hanno 3 opzioni:\n1. Aggiungere fattura manualmente: vi sarà richiesto di inserire l'importo da salvare e la data da associare a tale importo.\n2. Aggiungi foto fattura: vi sarà richiesto di inviare una foto dello scontrino di chiusura cassa in modo che provvederò io a ricavarmi l'importo e la data da salvare.\n3. Operazione: in questa sezione puoi chiedermi il fatturato di un giorno specifico o il fatturato di un intero mese. Puoi usare questa sezione anche per richiedere che venga eliminata una fattura in una particolare data in modo che non ne terrò conto per calcoli futuri.\n\n Inizia subito a tenere traccia del tuo fatturato!\nInvia un qualsiasi messaggio per iniziare.")
+            return await step_context.replace_dialog(WaterfallMain.__name__)
         else:
             return await step_context.replace_dialog(WaterfallMain.__name__)
 
